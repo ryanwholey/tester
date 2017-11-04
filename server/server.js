9020288e9d5860b4de83e13ae13b624430f79428
Ryan Wholey <rjwholey@gmail.com>
2017-11-04 16:40:34 -0700

    working server

---
+++ server/server.js
@@ -0,0 +1,53 @@
+var bodyParser = require('body-parser');
+var exphbs  = require('express-handlebars');
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan');
+var _ = require('lodash')
+
+var app = express();
+
+
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main',
+	layoutsDir: path.resolve(__dirname, '../client/templates'),
+}));
+
+app.set('view engine', 'handlebars');
+app.set('port', process.env.RWHOLEY_PORT || 8080);
+app.set('host', process.env.RWHOLEY_HOST || 'localhost');
+
+// middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse and set reqData
+app.use(function(req, res, next) {
+	req.reqData = {};
+
+	if (req.method === 'GET') {
+		req.reqData = req.query;
+	} else if (req.method === 'POST') {
+		req.reqData = _.assign({}, req.body);
+	}
+
+	next();
+});
+
+// routes
+app.use('/', function(req, res) {
+	res.render(path.resolve(__dirname, '../client/templates/home'), {
+		requestMethod: req.method,
+		data: req.reqData
+	});
+});
+
+app.listen(app.get('port'), app.get('host'), function(err) {
+	if (err) {
+		console.error('Error starting server...');
+		process.exit(1);
+	}
+
+	console.log('Server started at', app.get('host') + ':' + app.get('port'));
+});
